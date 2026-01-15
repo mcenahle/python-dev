29183320fef46cbeee0d58ff061e689fa609c889
mcenahle <i@mn.edu.kg>
2026-01-15 11:24:39 +0800

    同步MOOC的买课学习（1-3）

---
+++ MOOC/2. if/1.py
@@ -0,0 +1,5 @@
+word = "Nginx"
+if word in {"Mcenahle":100, "Nginx":0}:
+    print("1")
+else:
+    print("2")
---
+++ MOOC/3. 循环/1.py
@@ -0,0 +1,10 @@
+i = 5
+while i > 1:
+    i = i - 1
+    if i == 4:
+        continue
+    if i == 2:
+        break
+    print(i)
+i *= 2
+print(i)
